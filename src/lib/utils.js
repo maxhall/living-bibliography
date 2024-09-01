a140f90da31f40ff7b4cd136c7e07829a0495b07
Max Hall <maxwelljhall@gmail.com>
2024-09-01 22:40:09 +1000

    Broken graph first pass

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,3 +9,66 @@ export function parse_content_to_markdown(unsafe_content) {
 		USE_PROFILES: { html: true }
 	});
 }
+
+/** @param {import('./types').Living_Bibliography} bib */
+export function graph_data_from_bib_state(bib) {
+	/** @param {string} tag_title */
+	const get_tag_id = (tag_title) => {
+		const already_exists = nodes.find((n) => n.type == 'tag' && n.title == tag_title);
+
+		if (already_exists) return already_exists.id;
+
+		const id = last_id++;
+		nodes.push({
+			id,
+			type: 'tag',
+			radius: 1,
+			title: tag_title
+		});
+
+		return id;
+	};
+	/** @param {[number, number]} ids */
+	const add_link = ([a, b]) => {
+		const already_exists = links.find((l) => l.includes(a) && l.includes(b));
+
+		if (already_exists) return;
+
+		links.push([a, b]);
+	};
+
+	/** @type {{id: number, type: 'source' | 'tag', radius: number, title: string }[]} */
+	const nodes = [];
+	/** @type {[number, number][]}*/
+	const links = [];
+
+	let last_id = 0;
+
+	bib.entries.forEach((e) => {
+		const source_id = last_id++;
+
+		nodes.push({
+			id: source_id,
+			radius: 2,
+			type: 'source',
+			title: /** @type {string} */ (e.Title)
+		});
+
+		e.Tags.forEach((t) => {
+			const tag_id = get_tag_id(t);
+
+			add_link([source_id, tag_id]);
+		});
+	});
+
+	return {
+		nodes,
+		links: links.map((l) => {
+			return {
+				source: l[0],
+				target: l[1],
+				value: 1
+			};
+		})
+	};
+}
--- src/routes/Graph.svelte
+++ src/routes/Graph.svelte
@@ -1,66 +1,75 @@
 <script>
+	import { graph_data_from_bib_state } from '$lib/utils';
+	import { forceLink, forceManyBody, forceSimulation, forceX, forceY } from 'd3-force';
 	import { bib_state } from './state.svelte';
 	import { onMount } from 'svelte';
 
-	let last_id = 0;
+	let computed_nodes = $state([]);
+	let computed_links = $state([]);
 
 	onMount(() => {
 		if (!bib_state.bib) return;
 
-		/** @type {{id: number, type: 'source' | 'tag', title: string }[]} */
-		const nodes = [];
-		/** @type {[number, number][]}*/
-		const links = [];
+		const { nodes, links } = compute_positions(bib_state.bib);
 
-		/** @param {string} tag_title */
-		const get_tag_id = (tag_title) =>
-			nodes.find((n) => n.type == 'tag' && n.title == tag_title)?.id || last_id++;
+		computed_nodes = nodes;
+		computed_links = links;
 
-		/** @param {[number, number]} ids */
-		const add_link = ([a, b]) => {
-			const already_exists = links.find((l) => l.includes(a) && l.includes(b));
+		// console.log(computed_nodes);
+		// console.log(computed_links);
+	});
 
-			if (already_exists) return;
+	/** @param {import("$lib/types").Living_Bibliography} bib*/
+	function compute_positions(bib) {
+		const { nodes, links } = graph_data_from_bib_state(bib);
+		const simulation = forceSimulation(nodes)
+			.force(
+				'link',
+				forceLink(links)
+					.id((d) => d.id)
+					.strength(0.4)
+			)
+			.force('charge', forceManyBody())
+			.force('x', forceX().strength(0.2))
+			.force('y', forceY().strength(0.2));
 
-			links.push([a, b]);
-		};
-
-		bib_state.bib.entries.forEach((e) => {
-			const source_id = last_id++;
-
-			nodes.push({
-				id: source_id,
-				type: 'source',
-				title: /** @type {string} */ (e.Title)
-			});
-
-			e.Tags.forEach((t) => {
-				const tag_id = get_tag_id(t);
-
-				add_link([source_id, tag_id]);
-			});
+		simulation.on('tick', () => {
+			console.log('Tick!');
 		});
 
-		console.log(nodes);
-		console.log(links);
-	});
+		simulation.tick();
+
+		return {
+			nodes,
+			links
+		};
+	}
 </script>
 
 <figure>
-	<svg viewBox="0 0 100 100">
-		<!-- {#if nodes}
-			{#each nodes as n (n.index)}
-				<g
-					on:pointerenter={pause}
-					on:pointerleave={resume}
-					class="activity c-{n.type}"
-					transform="translate({n.x}, {n.y})"
-					in:fade|global={{ duration: 1500, delay: activity_type_params[n.type].intro }}
-				>
-					<circle r={n.r} />
-					<LabelLine title={n.title} {line_length} />
+	<svg viewBox="-50 -50 50 50">
+		{#if computed_links}
+			{#each computed_links as l}
+				<line x1={l.source.x} y1={l.source.y} x2={l.target.x} y2={l.target.y} />
+			{/each}
+		{/if}
+		{#if computed_nodes}
+			{#each computed_nodes as n}
+				<g class="activity c-{n.type}" transform="translate({n.x}, {n.y})">
+					<circle r={n.radius} />
 				</g>
 			{/each}
-		{/if} -->
+		{/if}
 	</svg>
 </figure>
+
+<style>
+	line {
+		stroke: var(--sandstone);
+		stroke-width: 0.3;
+	}
+
+	figure {
+		max-width: 25rem;
+	}
+</style>
