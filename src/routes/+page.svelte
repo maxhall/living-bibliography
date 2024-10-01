1ddbb181664efc28231fe7d6d21c8000ea29e778
Max Hall <maxwelljhall@gmail.com>
2024-10-01 14:09:16 +1000

    Preloaded server content working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
 <h1>Living bibliographies</h1>
-<p><a href="/viewer">Create your own</a></p>
+<h2>Create your own</h2>
+<p>You can <a href="/viewer">Create or view your own Living Bibliography.</a></p>
+<h2>Published bibliographies</h2>
+<ul>
+	<li><a href="/b/campus-collaboration">Campus collaboration</a></li>
+	<li><a href="/b/demonstration">Demonstration</a></li>
+</ul>
