70a6040012e15988116f88bd54eaaad76b6f4f06
Max Hall <maxwelljhall@gmail.com>
2026-04-28 16:06:42 +1000

    Update read-excel-sheet to address security

--- src/lib/xlsx/read_client.js
+++ src/lib/xlsx/read_client.js
@@ -1,28 +1,18 @@
-import readXlsxFileBrowser, { readSheetNames as readSheetNamesBrowser } from 'read-excel-file';
+import { readSheet } from 'read-excel-file/browser';
 
 /**
  * @param {ArrayBuffer} workbook
  **/
 export async function read_XLSX_browser(workbook) {
 	// TODO: Test what happens if we load an entirely empty workbook?
-	const sheet_names = await readSheetNamesBrowser(workbook);
-
 	/** @type {Partial<import('$lib/types').Workbook_Data>} */
 	const data = {
-		sheet_names
+		sheet_names: ['Info', 'Bibliography', 'Narrative']
 	};
 
-	data.info_rows = sheet_names.includes('Info')
-		? await readXlsxFileBrowser(workbook, { sheet: 'Info' })
-		: null;
-
-	data.bib_rows = sheet_names.includes('Bibliography')
-		? await readXlsxFileBrowser(workbook, { sheet: 'Bibliography' })
-		: null;
-
-	data.narrative_rows = sheet_names.includes('Narrative')
-		? await readXlsxFileBrowser(workbook, { sheet: 'Narrative' })
-		: null;
+	data.info_rows = await readSheet(workbook, 'Info') ?? null;
+	data.bib_rows = await readSheet(workbook, 'Bibliography') ?? null;
+	data.narrative_rows = await readSheet(workbook, 'Narrative') ?? null;
 
 	return /** @type {import('$lib/types').Workbook_Data} */ (data);
 }
--- src/lib/xlsx/read_server.js
+++ src/lib/xlsx/read_server.js
@@ -1,28 +1,18 @@
-import readXlsxFileNode, { readSheetNames as readSheetNamesNode } from 'read-excel-file/node';
+import { readSheet } from 'read-excel-file/node';
 
 /**
  * @param {Buffer} workbook
  **/
 export async function read_XLSX_server(workbook) {
 	// TODO: Test what happens if we load an entirely empty workbook?
-	const sheet_names = await readSheetNamesNode(workbook);
-
 	/** @type {Partial<import('$lib/types').Workbook_Data>} */
 	const data = {
-		sheet_names
+		sheet_names: ['Info', 'Bibliography', 'Narrative']
 	};
 
-	data.info_rows = sheet_names.includes('Info')
-		? await readXlsxFileNode(workbook, { sheet: 'Info' })
-		: null;
-
-	data.bib_rows = sheet_names.includes('Bibliography')
-		? await readXlsxFileNode(workbook, { sheet: 'Bibliography' })
-		: null;
-
-	data.narrative_rows = sheet_names.includes('Narrative')
-		? await readXlsxFileNode(workbook, { sheet: 'Narrative' })
-		: null;
+	data.info_rows = await readSheet(workbook, 'Info') ?? null;
+	data.bib_rows = await readSheet(workbook, 'Bibliography') ?? null;
+	data.narrative_rows = await readSheet(workbook, 'Narrative') ?? null;
 
 	return /** @type {import('$lib/types').Workbook_Data} */ (data);
 }
